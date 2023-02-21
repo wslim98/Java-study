850a9aa9f134ad14bb3ac3195770f9e2d042c1f4
wslim98 <105165202+wslim98@users.noreply.github.com>
2023-02-21 11:46:03 +0900

    Update q8.java

--- code/chap02/q8.java
+++ code/chap02/q8.java
@@ -1,2 +1,16 @@
-.
-.
+package chap02;
+
+import java.util.Scanner;
+
+public class q8 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("0~999 사이의 숫자를 입력하세요 : ");
+		
+		int num = in.nextInt();
+		
+		int result = num/100 + (num%100)/10 + num%10;
+		System.out.printf("각 자릿수의 합 = %d", result);
+	}
+}
