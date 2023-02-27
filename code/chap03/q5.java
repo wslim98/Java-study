00f20e1f9b89f3c0659fea1743b58fc0a14899aa
wslim98 <105165202+wslim98@users.noreply.github.com>
2023-02-28 04:04:20 +0900

    Create q5.java

---
+++ code/chap03/q5.java
@@ -0,0 +1,16 @@
+package chap03;
+
+public class q5 {
+
+	public static void main(String[] args) {
+		int a, b, c; // a + b > c
+		
+		for(a=1; a<10; a++)
+			for(b=1;b<10;b++)
+				for(c=1;c<10;c++)
+					if( (a*a+b*b==c*c) && (a+b+c<=20) ) {
+						System.out.printf("a=%d, b=%d, c=%d", a,b,c);
+						System.out.println();
+					}
+	}
+}
