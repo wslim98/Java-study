87e2f0cf5e489976fa29d2e7cf69f25473d832a1
wslim98 <105165202+wslim98@users.noreply.github.com>
2023-02-28 03:36:27 +0900

    Update q1.java

--- code/chap03/q1.java
+++ code/chap03/q1.java
@@ -1,1 +1,19 @@
-.
+package chap03;
+
+import java.util.Scanner;
+
+public class q1 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("나이 입력: ");
+		int age = in.nextInt();
+		
+		if(age>=19)
+			System.out.println("성년");
+		else
+			System.out.println("미성년");
+		
+		in.close();
+	}
+}
